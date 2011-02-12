17c73f9bd414e2e8f692f5de8fdc8c01c306c3c8
Sam Keen <sam@shopigniter.com>
2011-02-11 16:25:25 -0800

    output message for the index.php file

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 /* 
  * Simple setup file, Runs requirements check.  Will add more detail later 
  */
+echo "Welcome to Presta, running the requirements check...\n"
+
 require "lib/presta/Requirements.php";
 $mm = new Presta_Requirements();
 echo $mm->check();
