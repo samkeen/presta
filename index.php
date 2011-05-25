acc8e1dc2a40c975bdba656a61737b869e1e8a58
Sam Keen <sam@pageindigo.com>
2011-05-24 20:41:52 -0700

    Edited index.php via GitHub

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
  * Simple setup file, Runs requirements check.  Will add more detail later 
  */
 echo "Welcome to Presta, running the requirements check...\n";
+phpinfo();
 
-require "lib/presta/Requirements.php";
-$mm = new Presta_Requirements();
-echo $mm->check();
+//require "lib/presta/Requirements.php";
+//$mm = new Presta_Requirements();
+//echo $mm->check();
